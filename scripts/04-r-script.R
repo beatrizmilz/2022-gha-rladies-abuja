3564ebbd1b8f7e5d4095faa33d611322c0a5acb4
Beatriz Milz <milz.bea@gmail.com>
2022-10-28 22:09:09 -0300

    working on workflows

--- scripts/04-r-script.R
+++ scripts/04-r-script.R
@@ -5,12 +5,10 @@ print("Checking which files we have:")
 dir_ls()
 
 #  the URL 
-url_download <- "https://app.anm.gov.br/SIGBM/Publico/ClassificacaoNacionalDaBarragem/ExportarExcel"
+url_download = "https://app.anm.gov.br/SIGBM/Publico/ClassificacaoNacionalDaBarragem/ExportarExcel"
 
 # file name to save
-file_name <- paste0(
-  "sigbm_download_", Sys.Date(), ".xlsx"
-)
+file_name = paste0("sigbm_download_", Sys.Date(), ".xlsx")
 
 
 print("Making a POST request and writing file on disk:")
@@ -19,5 +17,3 @@ POST(url_download, write_disk(file_name))
 
 print("Checking which files we have:")
 dir_ls()
-
-     
